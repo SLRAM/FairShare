57ba5ba91c4b819e1a54fabeb226789205331ac1
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-05-12 03:34:22 -0400

    feat(profile): add profile view

---
+++ FairShare/UI/ProfileView.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileView.swift
+//  FairShare
+//
+//  Created by Stephanie Ramirez on 5/11/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+	@EnvironmentObject var viewModel: AuthViewModel
+	var body: some View {
+		if let user = viewModel.currentUser {
+			List {
+				Section {
+					HStack {
+						Text(user.initials)
+							.font(.title)
+							.fontWeight(.semibold)
+							.foregroundStyle(.white)
+							.frame(width: 72, height: 72)
+							.background(Color(.systemGray3))
+							.clipShape(Circle())
+
+						VStack(alignment: .leading, spacing: 5) {
+							Text(user.fullName)
+								.font(.subheadline)
+								.fontWeight(.semibold)
+								.padding(.top, 5)
+
+							Text(user.email)
+								.font(.footnote)
+								.foregroundStyle(.gray)
+						}
+					}
+				}
+
+				Section("Account") {
+					Button {
+						viewModel.signOut()
+					} label: {
+						SettingsRowView(rowType: .signOut)
+					}
+
+					Button {
+						print("Delete account")
+					} label: {
+						SettingsRowView(rowType: .delete)
+					}
+				}
+			}
+		}
+	}
+}
+
+#Preview {
+    ProfileView()
+}
