9107106b6a9c401c8353843bdc11f5f266259c94
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-05-25 20:55:12 -0400

    feat(ReceiptView): add camera and gallery access

---
+++ FairShare/UI/CameraView.swift
@@ -0,0 +1,44 @@
+//
+//  CameraView.swift
+//  FairShare
+//
+//  Created by Stephanie Ramirez on 5/25/24.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct CameraView: UIViewControllerRepresentable {
+	@Binding var selectedImage: UIImage?
+	@Environment(\.presentationMode) var isPresented
+
+	func makeUIViewController(context: Context) -> UIImagePickerController {
+		let imagePicker = UIImagePickerController()
+		imagePicker.sourceType = .camera
+		imagePicker.allowsEditing = true
+		imagePicker.delegate = context.coordinator
+		return imagePicker
+	}
+
+	func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+	}
+
+	func makeCoordinator() -> Coordinator {
+		return Coordinator(picker: self)
+	}
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+	var picker: CameraView
+
+	init(picker: CameraView) {
+		self.picker = picker
+	}
+
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let selectedImage = info[.originalImage] as? UIImage else { return }
+		self.picker.selectedImage = selectedImage
+		self.picker.isPresented.wrappedValue.dismiss()
+	}
+}
--- FairShare/UI/ReceiptView.swift
+++ FairShare/UI/ReceiptView.swift
@@ -5,34 +5,73 @@
 //  Created by Stephanie Ramirez on 5/11/24.
 //
 
-import SwiftUI
 import Firebase
+import PhotosUI
+import SwiftUI
 
 struct ReceiptView: View {
-	@EnvironmentObject var viewModel: AuthViewModel
-	@State var showActionSheet = false
+	@EnvironmentObject var authViewModel: AuthViewModel
+	@State private var showActionSheet = false
+	@State private var showCamera = false
+	@State private var showGallery = false
+	@State private var selectedImage: UIImage?
+	@State private var selectedItem: PhotosPickerItem?
+	@State private var imageToDisplay: Image?
 
 	var body: some View {
 		NavigationStack {
-			List(viewModel.receipts, id: \.id) { receipt in
+			//TODO: remove imageToDisplay. For testing purposes only.
+			imageToDisplay?
+				.resizable()
+				.scaledToFit()
+				.frame(width: 300, height: 300)
+
+			List(authViewModel.receipts, id: \.id) { receipt in
 				Text(receipt.date)
 			}
 			.navigationTitle("Previous Receipts")
 			.toolbar {
 				ToolbarItem(placement: .topBarTrailing) {
 					Button {
-						showActionSheet = true
+						showActionSheet.toggle()
 					} label: {
-						Label(title: { Text("Label") }, icon: { Image(systemName: "plus") })
+						Label(title: {}, icon: { Image(systemName: "plus") })
 					}
 				}
 			}
 			.confirmationDialog("New Receipt", isPresented: $showActionSheet) {
-				Button("Camera") { print("Camera") }
-				Button("Photo Gallery") { print("Photo Gallery") }
+				Button("Camera") { showCamera.toggle()}
+				Button("Photo Gallery") { showGallery.toggle()}
 				Button("Cancel", role: .cancel) { }
 			} message: {
 				Text("Please take a new photo or select an image from the image gallery")
+			}
+		}
+		.photosPicker(
+			isPresented: $showGallery,
+			selection: $selectedItem,
+			matching: .any(of: [.images, .screenshots, .livePhotos])
+		)
+		.fullScreenCover(
+			isPresented: $showCamera,
+			content: {
+				CameraView(selectedImage: self.$selectedImage)
+					.edgesIgnoringSafeArea(.all)
+			}
+		)
+		.onChange(of: selectedImage) {
+			//TODO: remove imageToDisplay. For testing purposes only.
+			if let image = selectedImage {
+				imageToDisplay = Image(uiImage: image)
+			}
+		}
+		.onChange(of: selectedItem) {
+			Task {
+				if let item = selectedItem {
+					if let data = try? await item.loadTransferable(type: Data.self) {
+						selectedImage = UIImage(data: data)
+					}
+				}
 			}
 		}
 	}
