bd7bf29dad4412f10e06ca1b60bebef387a4f4a5
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-05-25 23:29:51 -0400

    refactor(login view): consolidate strings

--- FairShare/Helpers/Constants.swift
+++ FairShare/Helpers/Constants.swift
@@ -36,11 +36,29 @@ enum ConstantColors {
 
 enum ConstantImages {
 	enum System {
+		static let arrowRight = ImageAsset(name: "arrow.right", isSystem: true)
 	}
 }
 
 enum ConstantStrings {
 	enum LoginView {
+		static let confirmString = "OK"
+		static let welcome = "Welcome Back"
+		static let register = "Register"
+		static let signIn = "Sign in"
+		static let signUp = "Sign up"
+		static let doesNotHaveAccount = "Don't have an account? "
+		static let hasAccount = "Already have an account? "
+
+		static let emailPlaceholder = "email@example.com"
+		static let passwordPlaceholder = "Enter your password"
+		static let namePlaceholder = "Enter your "
+
+		static let firstName = "First Name"
+		static let LastName = "Last Name"
+		static let email = "Email Address"
+		static let password = "Password"
+		static let confirmPassword = "Confirm Password"
 	}
 
 	enum MainTabView {
