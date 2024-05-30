a71419b791af71c0b25bd5acf4eb076339981968
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-05-29 20:00:00 -0600

    feat(cameraView): add scanning and displaying of image text

--- FairShare/Models/ReceiptModel.swift
+++ FairShare/Models/ReceiptModel.swift
@@ -15,3 +15,54 @@ struct ReceiptModel: Identifiable {
 	var tip: Double
 	var total: Double
 }
+
+protocol ReceiptText: Identifiable, Comparable {
+	var id: UUID { get }
+	var title: String { get set }
+
+}
+
+class ReceiptItem: ReceiptText {
+	let id: UUID
+	var title: String
+	var cost: Double
+
+	init(id: UUID = UUID(), title: String, cost: Double) {
+		self.id = id
+		self.title = title
+		self.cost = cost
+	}
+
+	var costAsCurrency: String {
+		return String(format: "$%.02f", self.cost)
+
+	}
+
+	static func == (lhs: ReceiptItem, rhs: ReceiptItem) -> Bool {
+		return lhs.id == rhs.id && lhs.title == rhs.title && lhs.cost == rhs.cost
+	}
+
+
+	static func < (lhs: ReceiptItem, rhs: ReceiptItem) -> Bool {
+		return lhs.id < rhs.id
+	}
+}
+
+class ReceiptInformation: ReceiptText {
+	let id: UUID
+	var title: String
+
+	init(id: UUID = UUID(), title: String) {
+		self.id = id
+		self.title = title
+	}
+
+	static func == (lhs: ReceiptInformation, rhs: ReceiptInformation) -> Bool {
+		return lhs.id == rhs.id && lhs.title == rhs.title
+	}
+
+	static func < (lhs: ReceiptInformation, rhs: ReceiptInformation) -> Bool {
+		return lhs.id < rhs.id
+	}
+
+}
--- FairShare/UI/Receipt/ReceiptView.swift
+++ FairShare/UI/Receipt/ReceiptView.swift
@@ -11,68 +11,40 @@ import SwiftUI
 
 struct ReceiptView: View {
 	@EnvironmentObject var authViewModel: AuthViewModel
-	@State private var showActionSheet = false
-	@State private var showCamera = false
-	@State private var showGallery = false
-	@State private var selectedImage: UIImage?
-	@State private var selectedItem: PhotosPickerItem?
-	@State private var imageToDisplay: Image?
+	
+	@State private var selectedReceipt: ReceiptModel? = nil
+	@State private var isPresented = false
 
 	var body: some View {
 		NavigationStack {
-			//TODO: remove imageToDisplay. For testing purposes only.
-			imageToDisplay?
-				.resizable()
-				.scaledToFit()
-				.frame(width: 300, height: 300)
-
 			List(authViewModel.receipts, id: \.id) { receipt in
 				Text(receipt.date)
+					.onTapGesture {
+						selectedReceipt = receipt
+					}
 			}
-			.navigationTitle(ConstantStrings.ReceiptView.navigationTitle)
+			.overlay(
+				Group {
+					if authViewModel.receipts.isEmpty {
+						Strings.ReceiptView.emptyState.text
+					}
+				}
+			)
 			.toolbar {
+				ToolbarItem(placement: .principal) {
+					Strings.ReceiptView.navigationTitle.text.font(.headline)
+				}
 				ToolbarItem(placement: .topBarTrailing) {
 					Button {
-						showActionSheet.toggle()
+						isPresented.toggle()
 					} label: {
-						ConstantImages.System.plus.image
+						Images.System.plus.image
 					}
 				}
 			}
-			.confirmationDialog("", isPresented: $showActionSheet) {
-				Button(ConstantStrings.ReceiptView.cameraButton) { showCamera.toggle()}
-				Button(ConstantStrings.ReceiptView.galleryButton) { showGallery.toggle()}
-				Button(ConstantStrings.ReceiptView.cancelButton, role: .cancel) { }
-			} message: {
-				ConstantStrings.ReceiptView.confirmationMessage.text
-			}
 		}
-		.photosPicker(
-			isPresented: $showGallery,
-			selection: $selectedItem,
-			matching: .any(of: [.images, .screenshots, .livePhotos])
-		)
-		.fullScreenCover(
-			isPresented: $showCamera,
-			content: {
-				CameraView(selectedImage: self.$selectedImage)
-					.edgesIgnoringSafeArea(.all)
-			}
-		)
-		.onChange(of: selectedImage) {
-			//TODO: remove imageToDisplay. For testing purposes only.
-			if let image = selectedImage {
-				imageToDisplay = Image(uiImage: image)
-			}
-		}
-		.onChange(of: selectedItem) {
-			Task {
-				if let item = selectedItem {
-					if let data = try? await item.loadTransferable(type: Data.self) {
-						selectedImage = UIImage(data: data)
-					}
-				}
-			}
+		.fullScreenCover(isPresented: $isPresented) {
+			NewReceiptView()
 		}
 	}
 }
