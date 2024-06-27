2e1688a730145b9df97c2c58b512b9c2f89778b6
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-06-26 20:00:00 -0600

    refactor(NewReceiptView): update to view model usage

--- FairShare/UI/Receipt/NewReceiptView.swift
+++ FairShare/UI/Receipt/NewReceiptView.swift
@@ -12,95 +12,72 @@ import SwiftUI
 struct NewReceiptView: View {
 	@Environment(\.dismiss) var dismiss
 	@EnvironmentObject var authViewModel: AuthViewModel
-
-	@State private var showActionSheet = true
-	@State private var showCamera = false
-	@State private var showGallery = false
-	@State private var selectedImage: UIImage?
-	@State private var selectedItem: PhotosPickerItem?
-//	@State private var receiptItems: [ReceiptItem] = []
-	@State private var receiptTexts: [any ReceiptText] = []
-
-	@State private var isEditing = false
-	@State private var showReceiptTab: Bool = false
-
-	//TODO: remove imageToDisplay and visualizedImage. For testing purposes only.
-	@State private var imageToDisplay: Image?
-	@State private var visualizedImage: UIImage?
+	@StateObject private var viewModel = NewReceiptViewModel()
 
 	var body: some View {
 		NavigationStack {
-			//TODO: remove imageToDisplay. For testing purposes only.
-//			if let currentImage = imageToDisplay {
-//				currentImage
-//					.resizable()
-//					.scaledToFit()
-//					.frame(width: 300, height: 300)
-//				ShareLink(item: currentImage, preview: SharePreview("Current Image", image: currentImage))
-//			}
-			
-			ReceiptItemsView(receiptTexts: $receiptTexts, isEditing: $isEditing)
-				.navigationBarTitleDisplayMode(.automatic)
+			VStack {
+				ReceiptItemsView(
+					itemsByType: viewModel.itemsByType(),
+					isEditing: $viewModel.isEditing,
+					receiptTexts: $viewModel.receiptTexts,
+					itemTapped: viewModel.itemTapped,
+					imageToDisplay: viewModel.imageToDisplay
+				)
 				.toolbar {
-					ToolbarItem(placement: .principal) {
-						Strings.NewReceiptView.navigationTitle.text.font(.headline)
-					}
-					if !receiptTexts.isEmpty {
+					if !viewModel.receiptTexts.isEmpty {
 						ToolbarItem(placement: .topBarTrailing) {
-							Button {
-								isEditing.toggle()
+							Button { viewModel.isEditing.toggle()
 							} label: {
 								Strings.NewReceiptView.editButton.text
 							}
 						}
 					}
 				}
-			
-			if !receiptTexts.isEmpty {
-				Button {
-					Task {
-						if let image = selectedImage {
-							try await authViewModel.createReceipt(from: receiptTexts, image: image)
-						}
-					}
 
-					dismiss()
-				} label: {
-					Strings.NewReceiptView.saveButton.text
+				if !viewModel.receiptTexts.isEmpty {
+					Button {
+						Task {
+							if let image = viewModel.selectedImage {
+								try await authViewModel.createReceipt(from: viewModel.receiptTexts, image: image)
+							}
+						}
+						dismiss()
+					} label: {
+						Strings.NewReceiptView.saveButton.text
+					}
 				}
 			}
 		}
-		.confirmationDialog("", isPresented: $showActionSheet) {
-			Button(Strings.NewReceiptView.cameraButton) { showCamera.toggle() }
-			Button(Strings.NewReceiptView.galleryButton) { showGallery.toggle() }
+		.confirmationDialog("", isPresented: $viewModel.showActionSheet) {
+			Button(Strings.NewReceiptView.cameraButton) { viewModel.showCamera.toggle() }
+			Button(Strings.NewReceiptView.galleryButton) { viewModel.showGallery.toggle() }
 			Button(Strings.NewReceiptView.cancelButton, role: .cancel) { dismiss() }
 		} message: {
 			Strings.NewReceiptView.confirmationMessage.text
 		}
 		.photosPicker(
-			isPresented: $showGallery,
-			selection: $selectedItem,
+			isPresented: $viewModel.showGallery,
+			selection: $viewModel.selectedItem,
 			matching: .any(of: [.images, .screenshots, .livePhotos])
 		)
 		.fullScreenCover(
-			isPresented: $showCamera,
+			isPresented: $viewModel.showCamera,
 			content: {
-				CameraView(recognizedTexts: self.$receiptTexts, visualizedImage: self.$visualizedImage, selectedImage: self.$selectedImage)
+				CameraView(recognizedTexts: $viewModel.receiptTexts, visualizedImage: $viewModel.visualizedImage, selectedImage: $viewModel.selectedImage)
 					.edgesIgnoringSafeArea(.all)
 			}
 		)
-		.onChange(of: selectedImage) {
-			//TODO: remove imageToDisplay. For testing purposes only.
-			///selectedImage will be saved for the final uploaded receipt.
-			if let image = selectedImage {
-				imageToDisplay = Image(uiImage: image)
+		.onChange(of: viewModel.selectedImage) {
+			if let image = viewModel.selectedImage {
+				viewModel.imageToDisplay = Image(uiImage: image)
 			}
 		}
-		.onChange(of: selectedItem) {
+		.onChange(of: viewModel.selectedItem) {
 			Task {
-				let image = await Processor.extractImage(from: selectedItem)
-				selectedImage = image
-				receiptTexts = Processor.recognizeText(from: image)
+				let image = await Processor.extractImage(from: viewModel.selectedItem)
+				viewModel.selectedImage = image
+				viewModel.receiptTexts = Processor.recognizeText(from: image)
 			}
 		}
 	}
--- FairShare/UI/Receipt/ReceiptItemsView.swift
+++ FairShare/UI/Receipt/ReceiptItemsView.swift
@@ -7,39 +7,64 @@
 
 import SwiftUI
 
-//TODO: rename ReceiptTextView
 struct ReceiptItemsView: View {
-	@Binding var receiptTexts: [any ReceiptText]
+	let itemsByType: [[ReceiptItem]]
 	@Binding var isEditing: Bool
+	@Binding var receiptTexts: [any ReceiptText]
+	let itemTapped: (ReceiptItem) -> Void
 
-	var receiptItems: [ReceiptItem] {
-		receiptTexts.compactMap { $0 as? ReceiptItem }
-	}
+	let imageToDisplay: Image?
 
 	var body: some View {
-		List {
-			ForEach(receiptItems, id: \.id) { receiptItem in
-				if let index = receiptTexts.firstIndex(where: { $0.id == receiptItem.id }) {
-					if isEditing {
-						EditableReceiptItemView(item: Binding(
-							get: { receiptTexts[index] as! ReceiptItem },
-							set: { receiptTexts[index] = $0 }
-						))
-					} else {
-						HStack {
-							Text(receiptItem.title)
-							Spacer()
-							Text(receiptItem.costAsCurrency)
+		GeometryReader { geometry in
+			List {
+				ForEach(itemsByType, id: \.self) { itemInType in
+					Section(header: Text(itemInType[0].type.rawValue)) {
+						ForEach(itemInType) { item in
+							if let index = receiptTexts.firstIndex(where: { $0.id == item.id }) {
+								if isEditing {
+									EditableReceiptItemView(item: Binding(
+										get: { receiptTexts[index] as! ReceiptItem },
+										set: { receiptTexts[index] = $0 }
+									))
+								} else {
+									Button {
+										print(item.title)
+										itemTapped(item)
+									} label: {
+										HStack {
+											Text(item.title)
+											Spacer()
+											Text(item.costAsCurrency)
+										}
+									}
+								}
+							}
 						}
 					}
 				}
+				if let currentImage = imageToDisplay {
+					Section(header: Strings.ReceiptDetailView.imagesSectionTitle.text) {
+						HStack {
+							Spacer()
+							currentImage
+								.resizable()
+								.aspectRatio(contentMode: .fit)
+								.frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.5)
+							ShareLink(item: currentImage, preview: SharePreview("Current Image", image: currentImage))
+							Spacer()
+						}
+						.frame(height: geometry.size.height * 0.5)
+						.background(Color(.systemGray5))
+					}
+				}
 			}
+			.tint(Color.black)
 		}
+		.listSectionSpacing(0)
+		.navigationTitle(Strings.NewReceiptView.navigationTitle.text)
+		.navigationBarTitleDisplayMode(.inline)
 	}
 }
 
-#Preview {
-	ReceiptItemsView(receiptTexts: .constant([
-		ReceiptInformation(title: "Fruit Shop")] + ReceiptItem.dummyArrayData
-	), isEditing: .constant(true))
-}
+//TODO: Add Preview
