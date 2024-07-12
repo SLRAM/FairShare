b8c85a171948f10f81ae032103e20b069ef48d0b
Stephanie Ramirez | Fueled <stephanie@fueled.com>
2024-07-11 20:00:00 -0600

    feat(stringToImage): create helper function to convert receipt items to image

--- FairShare/UI/Receipt/ReceiptDetailViewModel.swift
+++ FairShare/UI/Receipt/ReceiptDetailViewModel.swift
@@ -76,6 +76,102 @@ class ReceiptDetailViewModel: ObservableObject {
 		case .total:
 			return isEnabled ? currentUserTotal.currencyFormat() : item.cost.currencyFormat()
 
+	func guestReceiptImages() -> [UIImage]? {
+		if let guestReceiptDescription = guestReceiptDescription(), let receiptDescription = receiptDescription() {
+			return [guestReceiptDescription, receiptDescription]
+		} else {
+			return nil
 		}
+	}
+
+	func guestReceiptDescription() -> UIImage? {
+		var description = ""
+
+		if let guest = selectedGuest {
+			description += "Personal Receipt Breakdown for \(guest.fullName)\n\n"
+			let guestItems = receipt.currentUserReceipt(userID: guest.id).filter { $0.type == .item }
+			description += "Items:\n"
+			for item in guestItems {
+				let title = item.title
+				let guestCost = item.costPerPayer(guestCount: guestCount).currencyFormat()
+				description += "\(title), Cost: \(guestCost)\n"
+			}
+
+			description += "-------------------------------\n"
+
+			let guestSubtotal = receipt.userSubtotal(userID: guest.id).currencyFormat()
+			let guestServiceFee = receipt.userServiceTotal(userID: guest.id).currencyFormat()
+
+			let guestTaxTotal = receipt.userTaxTotal(userID: guest.id).currencyFormat()
+			let guestTotal = receipt.userTotal(userID: guest.id).currencyFormat()
+
+			description += "Subtotal: \(guestSubtotal)\n"
+			description += "Service Fee: \(guestServiceFee)\n"
+
+			description += "Tax Total: \(guestTaxTotal)\n"
+			description += "-------------------------------\n"
+			description += "Total: \(guestTotal)"
+		}
+
+		return description.image(size: CGSize(width: 300, height: 300))
+	}
+
+	func receiptDescription() -> UIImage? {
+		var description = "Original Receipt\n\n"
+		var itemDescription = ""
+		var subtotalDescription = ""
+		var taxDescription = ""
+		var tipDescription = ""
+		var totalDescription = ""
+		let divider = "-------------------------------\n"
+
+		for itemInType in groupedItems {
+			guard let firstItem = itemInType.first else { continue }
+
+			switch firstItem.type {
+			case .item:
+				itemDescription += "Items:\n"
+				for item in itemInType {
+					let title = item.title
+					let cost = item.cost.currencyFormat()
+					itemDescription += "\(title), Cost: \(cost)\n"
+				}
+				
+				itemDescription += divider
+
+			case .subTotal:
+				for item in itemInType {
+					let cost = item.cost.currencyFormat()
+					subtotalDescription += "Subtotal: \(cost)\n"
+				}
+			case .service:
+				for item in itemInType {
+					let cost = item.cost.currencyFormat()
+					subtotalDescription += "Service Fee: \(cost)\n"
+				}
+
+			case .tax:
+				for item in itemInType {
+					let cost = item.cost.currencyFormat()
+					taxDescription += "Tax: \(cost)\n"
+				}
+
+			case .tip:
+				for item in itemInType {
+					let cost = item.cost.currencyFormat()
+					tipDescription += "Tip: \(cost)\n"
+				}
+
+			case .total:
+				for item in itemInType {
+					let cost = item.cost.currencyFormat()
+					totalDescription += "Total: \(cost)\n"
+				}
+			}
+		}
+
+		description += itemDescription + subtotalDescription + taxDescription + tipDescription + divider + totalDescription
+
+		return description.image(size: CGSize(width: 300, height: 300))
 	}
 }
